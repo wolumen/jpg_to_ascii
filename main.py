c1ece6f67f7773ecd88e835342e5105d22484a7f
wolumen <wolumen@web.de>
2021-02-25 20:14:35 +0000

    Größe des new_img

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ width, height = img.size
 aspect_ratio = height / width
 
 #calculate new hight
-new_width = 120
-new_height = aspect_ratio * new_width
+new_width = 140
+new_height = int(aspect_ratio * new_width)
 
-img = img.resize((new_width, int(new_height)))
+img = img.resize((new_width, (new_height)))
+print(new_height, new_width)
 
 # convert each pixel to grayscale
 img = img.convert('L')
@@ -34,7 +35,7 @@ new_pixels_count = len(new_pixels)
 ascii_image = [new_pixels [index:index + new_width] for index in range (0, new_pixels_count, new_width)]
 
 ascii_image = "\n".join(ascii_image)
-print(ascii_image)
+#print(ascii_image)
 
 with open("ascii_image.txt", "w") as file:
   file.write(ascii_image)
@@ -48,16 +49,16 @@ with open("ascii_image.txt", "w") as file:
 
 # resize
 #I managed to save pic to jpg
-im = Image.new("RGB", (width*20, height*20), (256,256,256))
+im = Image.new("RGB", (10*new_width, 10*new_height), (256,256,256))
 
 # get a font
-font = ImageFont.truetype("FreeMono.ttf", 40)
+font = ImageFont.truetype("FreeMono.ttf", 10)
 
 # get a drawing context
 d = ImageDraw.Draw(im)
 
 # draw multiline text
-d.multiline_text((0, 0), ascii_img_out, font=font, fill=(0, 0, 0))
+d.multiline_text((0, 0), ascii_image, font=font, fill=(0, 0, 0))
 
 # save Image
 im.save("new_img.jpg")
